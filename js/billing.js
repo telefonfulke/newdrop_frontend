e574282e2a37a01da01218188498de434f92cb48
telefonfulke <lacinagy2005@gmail.com>
2025-03-12 11:25:31 +0100

    checkout

--- js/billing.js
+++ js/billing.js
@@ -34,24 +34,7 @@ window.paypal
 
         async createOrder() {
             try {
-                const response = await fetch("/api/orders", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    // use the "body" param to optionally pass additional order information
-                    // like product ids and quantities
-                    body: JSON.stringify({
-                        cart: [
-                            {
-                                id: "YOUR_PRODUCT_ID",
-                                quantity: "YOUR_PRODUCT_QUANTITY",
-                            },
-                        ],
-                    }),
-                });
-
-                const orderData = await response.json();
+                let orderData = JSON.parse(localStorage.getItem("cart")) || [];
 
                 if (orderData.id) {
                     return orderData.id;
